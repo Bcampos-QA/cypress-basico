ba63f290b98bd67e7f09dad1a8043cc59b365482
Bruno Guerreiro <brunoguerreiro.campos@hotmail.com>
2025-06-02 17:52:29 -0300

    Adiciona arquivo de suporte padrão

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // configure event listeners aqui, se precisar
-    },
-    specPattern: 'cypress/integration/**/*.spec.js',
-    baseUrl: './src/index.html', // ou a URL da sua aplicação
-    supportFile: false, // <---- desativa o support file
-  }
+  viewportHeight: 880,
+  viewportWidth: 1280,
+  e2e: {},
 })
