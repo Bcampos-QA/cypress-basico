2f2127403d3c32cb91725e6575485b67a04679a0
Bruno Campos <brunoguerreiro.campos@hotmail.com>
2025-07-03 18:19:49 -0300

    Atualização de testes 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {
-    supportFile: false,
-    specPattern: 'cypress/integration/CAC-TAT.spec*.js', // aqui você diz onde os testes estão
-  },
-});
+  e2e: {},
+})
