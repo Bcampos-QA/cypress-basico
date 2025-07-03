e8047a1826d38000ecf5bdb527b72087cd27fb0b
Bruno Campos <brunoguerreiro.campos@hotmail.com>
2025-07-03 18:03:41 -0300

    Atualização de testes 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
-})
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
   e2e: {
-    supportFile: false,
+    supportFile: false, // ou substitua por 'cypress/support/e2e.js' se quiser usar
   },
 });
